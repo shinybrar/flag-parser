baabb4efafb138f534d1d48ba4a50b543225a944
Shiny Brar <shiny@psrcontrol.physics.mcgill.ca>
2016-10-31 23:55:01 -0400

    tcpServer working. -SB

--- python/tcpClient.py
+++ python/tcpClient.py
@@ -2,14 +2,16 @@
 
 import socket
 
-
-TCP_IP = '132.206.126.213'
+TCP_IP = ''
 TCP_PORT = 5555
 BUFFER_SIZE = 1024
+
 MESSAGE = '{"runCommand":"./pcapParser -d eth0 -c 10 -p 0 --binary"}'
 
+print("Starting Client...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
+print("sending command string")
 s.send(MESSAGE)
 
 while True:
--- python/tcpServer.py
+++ python/tcpServer.py
@@ -6,67 +6,61 @@ import os
 import sys
 import json
 import subprocess
-from sendfile import sendfile
 
 ################################################################################
-
-#Moving into build directory
-os.chdir("../build/")
-if (not os.path.isfile('pcapParser')):
-    print "ERROR: Could not find file pcapParser in ../build directory.\
+def changeDir():
+    #Moving into build directory
+    os.chdir("../build/")
+    if (not os.path.isfile('pcapParser')):
+        print "ERROR: Could not find file pcapParser in ../build directory.\
             Build the project before running the TCP Server"
-    exit()
+        exit()
 ################################################################################
+def startServer( ip, port, buffer ):
+    #TCP Socket Parameters - Change as Needed
+    TCP_IP      = ip  	# '' Symbolic name, meaning all available interfaces
+    TCP_PORT    = port
+    BUFFER_SIZE = buffer
 
-#TCP Socket Parameters - Change as Needed
-TCP_IP      = ''    # Symbolic name, meaning all available interfaces
-TCP_PORT    = 5555
-BUFFER_SIZE = 1024
+    #Creating a TCP Socket
+    print "Starting TCP Server\n"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
 
-################################################################################
+    #Start Listening with a backlog of 5 connections
+    s.listen(5) 
 
-#Creating a TCP Socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-
-#Start Listening with a backlog of 1 connections
-s.listen(1)
-
-while True:
-    #Accepting Connections
-    connection, address = s.accept()
-
-    # Example JSON string command
-    # '{"runCommand":"./pcapParser -d eth0 -c 10 -p 0 --binary"}'
-
-    print 'Connection Address: ', address
+    print("Server started, listening ...")
     while True:
-        data = connection.recv(BUFFER_SIZE)
-        if not data:
-            break
-        #Load the JSON Data
-        jsonString = json.loads(data)
-        print "Received Command:", data
-    
-        #Run the JSON Command
-        process = subprocess.call(jsonString['runCommand'], shell=True)
+        #Accepting Connections
+        connection, address = s.accept()
 
-        if (not os.path.isfile('packetData.bin')):
-            connection.send("ERROR: Could not find file packetData.bin")
+        # Example JSON string command
+        # '{"runCommand":"./pcapParser -d eth0 -c 10 -p 0 --binary"}'
 
-        fd = open("packetData.bin","rb")
-        blocksize = os.path.getsize("packetData.bin")
-        offset = 0
-
+        print 'Connection Address: ', address
         while True:
-            sent = sendfile(s.fileno(), fd.fileno(), offset, blocksize)
-            if sent == 0:
-                break  # EOF
-        offset += sent
-        
-        # with open("packetData.bin", "rb") as fd:
-        #     buf = fd.read(1024)
-        #     while (buf):
-        #         s.send(buf)
-        #         buf = fd.read(1024)  
+            data = connection.recv(BUFFER_SIZE)
+            if not data:
+                break
+            #Load the JSON Data
+            jsonString = json.loads(data)
+            print "Received Command:", data
+    
+            #Run the JSON Command
+            process = subprocess.call(jsonString['runCommand'], shell=True)
+
+            if (not os.path.isfile('packetData.bin')):
+                connection.send("ERROR: Could not find file packetData.bin")
+
+            fd = open("packetData.bin","rb") 	#Open file in binary
+	    binData = fd.read(1024)
+	    while (binData):
+	        s.send(binData)
+                binData = fd.read(1024)
+	#Terminate Current Connection but keep the server going.  
         connection.close()
+################################################################################
+if __name__ == "__main__":
+    changeDir()
+    startServer('', 5555, 1024)
